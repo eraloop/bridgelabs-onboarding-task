5bf43d5e8e44b7b67bdedf6f3f44101cc3e6f5d1
era <romanricakam@gmail.com>
2022-02-04 18:02:06 +0100

    cleanup

---
+++ google-oauth/src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+      <h1>this is a test message</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+ 
+}
+</script>
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
