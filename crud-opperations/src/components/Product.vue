cfcd8b08a6820a32a2c493eacbf27936148206dc
era <romanricakam@gmail.com>
2022-02-14 04:35:10 +0100

    added some third party packages, done some initial component structure

---
+++ crud-opperations/src/components/Product.vue
@@ -0,0 +1,33 @@
+<template>
+   <div class="container">
+       <h3>this is the product</h3>
+   </div>
+</template>
+<script>
+   export default{
+       components:{
+            
+       },
+       data(){
+           return{
+               
+              }
+       },
+       computed:{
+           
+        },
+       methods:{
+           
+       },
+       mounted: function(){
+            
+       }
+    }
+</script>
+<style scoped> 
+   *{
+        padding: 0;
+        margin: 0;
+        box-sizing:border-box;
+    }
+</style>
