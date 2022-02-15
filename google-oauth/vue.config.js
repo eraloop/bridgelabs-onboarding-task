8bf5aaff90c497c71a8661301095d5c3e0b62c70
era <romanricakam@gmail.com>
2022-02-15 20:11:17 +0100

    done

--- google-oauth/vue.config.js
+++ google-oauth/vue.config.js
@@ -1,5 +1,7 @@
-export const devServer = {
-    host: '127.0.0.1',
-    port: 8080,
-    public: 'localhost:8080',
-};
+module.exports = {
+    devServer: {
+        host: '127.0.0.1',
+        port: 8080,
+        public: 'localhost:8080',
+    },
+}
