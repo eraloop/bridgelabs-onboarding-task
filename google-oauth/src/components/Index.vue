23316c50c2a6863ac7cbd6a21f55ae6cfe483e17
era <romanricakam@gmail.com>
2022-02-15 19:25:37 +0100

    upgrade

--- google-oauth/src/components/Index.vue
+++ google-oauth/src/components/Index.vue
@@ -1,7 +1,14 @@
 <template>
     <div class="container">
-        <h2> Welcome to our OAuth Login Screen </h2>
-        <div class="g-signin2" data-onsuccess="onSignIn"></div>
+       <div class="content p-4 ">
+           <div class="google-form text-center justify-content-center align-items-center">
+                <h2 class="p-2"> Welcome to our OAuth Login Screen </h2>
+                <div class="g-signin2 m-2 text-center" data-onsuccess="onSignIn"></div>
+           </div>
+           <div class="custom">
+               <p>Don't have a google account? </p><a target="blank" href="www.google.com">Create One</a>
+           </div>
+       </div>
     </div>
 </template>
 <script>
@@ -54,6 +61,31 @@
         height: 100vh;
         width: 100%
     }
+    
+    .content{
+        /* border: 1px solid black ; */
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+        width: 40%;
+        height: 40%;
+        padding: 20px;
+        border-radius: 10px ;
+        outline: none;
 
+    }
+    .google-form{
+        padding: 10px 0 20px;
+        margin-bottom: 30px;
+        text-align: center;
+        justify-content: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .custom{
+        padding: 10px 0;
+        margin: 10px 0;
+        color:#2c3e50;
+    }
 
 </style>
