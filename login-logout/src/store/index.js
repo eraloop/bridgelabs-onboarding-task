812fe5e011553f20b6476d6b810d65b3e762cdcb
era <romanricakam@gmail.com>
2022-02-23 11:55:39 +0100

    working with previous commit , this version is more stable to continue building from

--- login-logout/src/store/index.js
+++ login-logout/src/store/index.js
@@ -2,33 +2,27 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import  RegisterService  from '../services/register'
 import  LoginService  from '../services/login'
+<<<<<<< HEAD
 // import  LogoutService  from '../services/logout'
 import TokenService from '../services/storage'
 import router from '../router'
+=======
+import  LogoutService  from '../services/logout'
+>>>>>>> parent of 59d62774... successfully implemented the login and register apis, looking to do the logout tomorrow
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    view_name: "Login",
-    registrationSuccess:{},
-    userData:{
-      name:"",
-      fname: "",
-      lname: "",
-      email: "",
-      isAdmin: ""
-    },
+    view_name: "",
+    registrationSuccess:{
 
-    // authentication variables
-    token: TokenService.getToken(),
-    refreshToken: "",
-    accessToken: ""
+    }
   },
   mutations: {
-
     update_name (state, payload){
       state.view_name = payload
+<<<<<<< HEAD
     },
 
     loginSuccess(state, payload){
@@ -51,51 +45,60 @@ export default new Vuex.Store({
       state.userData.email = payload.email
       state.userData.isAdmin = payload.isAdmin
       state.userData.name = payload.name
+=======
+>>>>>>> parent of 59d62774... successfully implemented the login and register apis, looking to do the logout tomorrow
     }
   },
 
   getters:{
     get_view_name: (state)=> {
       return state.view_name
+<<<<<<< HEAD
     },
     get_user_data(state){
       return state.userData
     },
     get_token(state){
       return state.token
+=======
+>>>>>>> parent of 59d62774... successfully implemented the login and register apis, looking to do the logout tomorrow
     }
   },
 
   actions: {
     async login({ commit }, payload) {
       try {
-        const token = await LoginService.loginUser(payload)
-        commit("loginSuccess", token)
+        const token = await LoginService.login(payload.username, payload.password)
+        commit.push("true")
         console.log(token)
-        router.push('/welcome')
-        return true
+        return payload
       } catch (e) {
         console.log(e)
         return false
       }
     },
 
-    async register({commit},user) {
+    async register(user) {
       try {
         const token = await RegisterService.registerUser(user)
-        commit('registrationSuccess', token)
+        // commit('registrationSuccess', (token, user.username))
         console.log(token)
-        router.push('/welcome')
         return true
       } catch (e) {
         console.log("console from register store", e)
         return false
       }
     },
+<<<<<<< HEAD
     
     async logout() {
       console.log(this.refreshToken)
       // await LogoutService.logoutUser(refreshToken)
+=======
+
+    logout() {
+      LogoutService.logout()
+>>>>>>> parent of 59d62774... successfully implemented the login and register apis, looking to do the logout tomorrow
     },
   },
 })
