a7dd2e67f7161329c10d08734bbee017b65e15b0
er@ <romanricakam@gmail.com>
2022-02-25 02:34:16 +0100

    Revert "resolved some minor file differences "

--- crud-opperations/.eslintrc.js
+++ crud-opperations/.eslintrc.js
@@ -1,23 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    root: true,
+    env: {
+      node: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
+    'extends': [
+      'plugin:vue/essential',
+      'eslint:recommended'
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    parserOptions: {
+      parser: 'babel-eslint'
     },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
     }
-};
+  }
+  
---
+++ google-oauth/src/views/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <Index/>
+    </div>
+</template>
+<script>
+
+import Index from '@/components/Index.vue'
+   export default{
+       components:{
+            Index
+       },
+       data(){
+           return{
+               
+              }
+       },
+       computed:{
+           
+        },
+       methods:{
+           
+       },
+       mounted: function(){
+            
+       }
+    }
+</script>
+<style scoped> 
+
+</style>
