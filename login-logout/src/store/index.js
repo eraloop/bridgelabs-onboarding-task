b6d3037db3a62099d86241a9163abd5c1b4c7617
era <romanricakam@gmail.com>
2022-02-25 02:02:36 +0100

    resolving files and packages

--- login-logout/src/store/index.js
+++ login-logout/src/store/index.js
@@ -2,13 +2,9 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import  RegisterService  from '../services/register'
 import  LoginService  from '../services/login'
-<<<<<<< HEAD
 // import  LogoutService  from '../services/logout'
 import TokenService from '../services/storage'
 import router from '../router'
-=======
-import  LogoutService  from '../services/logout'
->>>>>>> parent of 59d62774... successfully implemented the login and register apis, looking to do the logout tomorrow
 
 Vue.use(Vuex)
 
@@ -22,7 +18,6 @@ export default new Vuex.Store({
   mutations: {
     update_name (state, payload){
       state.view_name = payload
-<<<<<<< HEAD
     },
 
     loginSuccess(state, payload){
@@ -45,23 +40,18 @@ export default new Vuex.Store({
       state.userData.email = payload.email
       state.userData.isAdmin = payload.isAdmin
       state.userData.name = payload.name
-=======
->>>>>>> parent of 59d62774... successfully implemented the login and register apis, looking to do the logout tomorrow
     }
   },
 
   getters:{
     get_view_name: (state)=> {
       return state.view_name
-<<<<<<< HEAD
     },
     get_user_data(state){
       return state.userData
     },
     get_token(state){
       return state.token
-=======
->>>>>>> parent of 59d62774... successfully implemented the login and register apis, looking to do the logout tomorrow
     }
   },
 
@@ -89,16 +79,9 @@ export default new Vuex.Store({
         return false
       }
     },
-<<<<<<< HEAD
-    
-    async logout() {
-      console.log(this.refreshToken)
-      // await LogoutService.logoutUser(refreshToken)
-=======
 
     logout() {
       LogoutService.logout()
->>>>>>> parent of 59d62774... successfully implemented the login and register apis, looking to do the logout tomorrow
     },
   },
 })
